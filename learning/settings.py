300e1bd15b5546dfeb6830191d73fa997b26007d
bragacatalin1983 <52997575+bragacatalin1983@users.noreply.github.com>
2021-01-27 16:23:39 +0200

    Update settings.py

--- learning/settings.py
+++ learning/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,4 +134,4 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sectiaimpr.casatineretului@gmail.com'
 EMAIL_HOST_PASSWORD = 'davele20'
-django_heroku.settings(locals())
+
